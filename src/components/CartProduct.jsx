064a73277cc2a0b1b5ba27ef1675a1bd5782aacc
tee2490@hotmail.com <tee2490@hotmail.com>
2024-06-07 22:12:17 +0700

    Manage Cart Product

--- src/components/CartProduct.jsx
+++ src/components/CartProduct.jsx
@@ -1,12 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { remove } from "../store/cartSlice";
+import { Button, Card, Text } from "react-native-paper";
 
-export default function CartProduct() {
+export default function CartProduct({ item }) {
+  const { id, name, price, image } = item;
+  const dispatch = useDispatch();
+
   return (
-    <View>
-      <Text>CartProduct</Text>
+    <View style={styles.container}>
+      <Card mode="contained">
+        <Card.Cover source={image} />
+        <Card.Content>
+          <Text variant="bodyMedium">{name}</Text>
+          <Text variant="titleMedium">${price}</Text>
+        </Card.Content>
+        <Card.Actions>
+          <Button
+            buttonColor="brown"
+            mode="contained"
+            onPress={() => dispatch(remove(item))}
+          >
+            Remove
+          </Button>
+        </Card.Actions>
+      </Card>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 5,
+    marginVertical: 10,
+  },
+});
