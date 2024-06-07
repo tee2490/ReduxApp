3a6a9c9b9aa6e02782d0486126ec151845ce08fb
tee2490@hotmail.com <tee2490@hotmail.com>
2024-06-07 21:07:07 +0700

    Slice and Action

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navigators from "./src/screen/Navigators";
+import { Provider } from "react-redux";
+import { store } from "./src/store/store";
 
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <Navigators />
-    </>
+    </Provider>
   );
 }
