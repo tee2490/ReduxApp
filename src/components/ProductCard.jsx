e32c2a35d4067570725c4fd715a8e8dafd1881af
tee2490@hotmail.com <tee2490@hotmail.com>
2024-06-07 20:29:03 +0700

    Manage Product Card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import { Button, Card, Text } from "react-native-paper";
 
-export default function ProductCard() {
+export default function ProductCard({ item }) {
+  const { id, name, price, image } = item;
+
   return (
-    <View>
-      <Text>ProductCard</Text>
+    <View style={styles.container}>
+      <Card mode="contained">
+        <Card.Cover source={image} />
+        <Card.Content>
+          <Text variant="bodyMedium">{name}</Text>
+          <Text variant="titleMedium">${price}</Text>
+        </Card.Content>
+        <Card.Actions>
+          <Button mode="contained">Add To Cart</Button>
+        </Card.Actions>
+      </Card>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 5,
+    marginVertical: 5,
+  },
+});
